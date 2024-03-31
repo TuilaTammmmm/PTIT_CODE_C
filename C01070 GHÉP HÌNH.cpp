0a1d2f71d59d8214a2d3c491ed6a7f18b77aa75a
TuiLaTammmmm <dragonkiller2k5@gmail.com>
2024-03-31 09:50:21 +0700

    commit

--- C01070 GHÉP HÌNH.cpp
+++ C01070 GHÉP HÌNH.cpp
@@ -1,26 +1,23 @@
 #include <stdio.h>
-
+#include <math.h>
+int Shcn(int a,int b)
+{
+    int S=a*b;
+    return S;
+}
 int main() {
     int r1, r2, r3, c1, c2, c3;
     scanf("%d %d", &r1, &c1);
     scanf("%d %d", &r2, &c2);
     scanf("%d %d", &r3, &c3);
-   if ((c1 == c2 && c2 == c3) && (r1 + r2 + r3 == c1)) {
-    printf("YES\n");
-} else if ((c1 == r2 && c2 == c3) && (r1 + c2 + r3 == c1)) {
-    printf("YES\n");
-} else if ((c1 == r2 && r2 == r3) && (r1 + c2 + c3 == c1)) {
-    printf("YES\n");
-} else if ((r1 == r2 && r2 == c3) && (c1 + c2 + r3 == r1)) {
-    printf("YES\n");
-} else if ((r1 == c2 && r2 == c3) && (c1 + r2 + r3 == r1)) {
-    printf("YES\n");
-} else if ((r1 == c2 && r2 == r3) && (c1 + c2 + r3 == r1)) {
-    printf("YES\n");
-} else if ((r1 == r2 && r2 == c3) && (c1 + r2 + c3 == r1)) {
-    printf("YES\n");
-} else {
-    printf("NO\n");
-}
+    int S1=Shcn(r1,c1);
+    int S2=Shcn(r2,c2);
+    int S3=Shcn(r3,c3);
+    int TongDienTich=S1+S2+S3;
+    int N=sqrt(TongDienTich);
+    if (N*N==TongDienTich)
+    {printf("YES\n");}
+    else
+    {printf("NO\n");}
     return 0;
 }
